33cd03c762fa6464f36303227fb00da6f5d5cb66
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-09-09 19:56:25 +0000

    Main Upload
    Everything fleshed out so far, waiting for possible advice from instructor

--- main.cs
+++ main.cs
@@ -1,7 +1,20 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    private const double BASE_RATE = 200;
+    private const double RATE_PER_HOUR = 150;
+    private const double RATE_PER_MILE = 2;
+
+    static void Main()
+    {
+        Console.Write("Enter hours: ");
+        double hours = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter miles: ");
+        double miles = double.Parse(Console.ReadLine());
+
+        double fee = BASE_RATE + RATE_PER_HOUR * hours + RATE_PER_MILE * miles;
+        Console.WriteLine($"Total fee: ${fee:0.00}");
+    }
 }
